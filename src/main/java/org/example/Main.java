7ad9ef16b5bf32164c3eafd0210f13a5f7d61672
unknown <stepan.legin2018@yandex.ru>
2025-04-12 10:11:47 +0300

    Add new class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
         System.out.println("Second commit!");
 
+        Cat firstCat = new Cat("Борис", "бежевый", 2);
+        Cat secondCat = new Cat("Мурзик", "чёрно-белый", 3);
+
+        System.out.println("Данные первого кота: " + firstCat.getName() + " , " + firstCat.getColor() +
+                " , " + firstCat.getAge());
+        System.out.println("Данные второго кота: " + secondCat.getName() + " , " + secondCat.getColor() +
+                " , " + secondCat.getAge());
     }
 }
